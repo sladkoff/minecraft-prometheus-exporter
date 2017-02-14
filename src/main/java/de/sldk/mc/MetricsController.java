722ec70907572dd5392114e20fc9e949db2c630e
Leonid Koftun <leonid.koftun@gmail.com>
2017-02-14 21:26:51 +0100

    Export JVM memory usage

--- src/main/java/de/sldk/mc/MetricsController.java
+++ src/main/java/de/sldk/mc/MetricsController.java
@@ -20,6 +20,7 @@ public class MetricsController extends AbstractHandler {
     private Gauge playersOnline = Gauge.build().name("mc_players_online_total").help("Players currently online per world").labelNames("world").create().register();
     private Gauge entities = Gauge.build().name("mc_entities_total").help("Entities loaded per world").labelNames("world").create().register();
     private Gauge livingEntities = Gauge.build().name("mc_living_entities_total").help("Living entities loaded per world").labelNames("world").create().register();
+    private Gauge memory = Gauge.build().name("mc_jvm_memory").help("JVM memory usage").labelNames("type").create().register();
 
     @Override
     public void handle(String s, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -33,6 +34,9 @@ public class MetricsController extends AbstractHandler {
             entities.labels(world.getName()).set(world.getEntities().size());
             livingEntities.labels(world.getName()).set(world.getLivingEntities().size());
         }
+
+        memory.labels("max").set(Runtime.getRuntime().maxMemory());
+        memory.labels("free").set(Runtime.getRuntime().freeMemory());
 
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType(TextFormat.CONTENT_TYPE_004);
