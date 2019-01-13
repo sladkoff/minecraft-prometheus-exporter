1dbd5d45ab55fa8ee79a48220904af7096123862
Lukas Fülling <lukas@k40s.net>
2019-01-13 04:52:59 +0100

    add ability to set host

--- src/main/java/de/sldk/mc/PrometheusExporter.java
+++ src/main/java/de/sldk/mc/PrometheusExporter.java
@@ -5,6 +5,8 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.eclipse.jetty.server.Server;
 
+import java.net.InetSocketAddress;
+
 public class PrometheusExporter extends JavaPlugin {
 
     FileConfiguration config = getConfig();
@@ -19,6 +21,7 @@ public class PrometheusExporter extends JavaPlugin {
                 .getScheduler()
                 .scheduleSyncRepeatingTask(this, tpsPoller, 0, TpsPoller.POLL_INTERVAL);
 
+        PluginConfig.HOST.setDefault(config);
         PluginConfig.PORT.setDefault(config);
         PluginConfig.PLAYER_METRICS.setDefault(config);
 
@@ -26,19 +29,20 @@ public class PrometheusExporter extends JavaPlugin {
         saveConfig();
 
         int port = PluginConfig.PORT.get(config);
+        String host = PluginConfig.HOST.get(config);
         boolean individualPlayerMetrics = PluginConfig.PLAYER_METRICS.get(config);
 
         if (individualPlayerMetrics) {
             getLogger().warning("Flag '" + PluginConfig.PLAYER_METRICS.getKey() + "' is enabled. This option is not recommended for public servers!");
         }
 
-        server = new Server(port);
+        InetSocketAddress address = new InetSocketAddress(host, port);
+        server = new Server(address);
         server.setHandler(new MetricsController(this, individualPlayerMetrics));
 
         try {
             server.start();
-
-            getLogger().info("Started Prometheus metrics endpoint on port " + port);
+            getLogger().info("Started Prometheus metrics endpoint at: " + host + ":" + port);
 
         } catch (Exception e) {
             getLogger().severe("Could not start embedded Jetty server");
