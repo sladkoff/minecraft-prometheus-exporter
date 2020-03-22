d14fa1908e275d3db8877e3760c7ba37b83255d8
aSemy <adam.semenenko@gmail.com>
2020-03-22 11:07:38 +0100

    mc_entities_total - add entity type, and alive (#29)
    * Add entity type and aliveness to entity metric. Unit test. Adjust pom to let maven run tests, silence encoding warning.
    
    * improve test, share code. And add manual override, because armour stands should not be considered alive.
    
    * Add 'spawnable' label to entities metric
    * use 'key' for entity type name, and handle unknown 'key' error
    * alive override - update name and use singletonMap
    * remove mc_living_entities_total - duplicated by mc_entities_total
    * update README to reflect mc_living_entities_total removed
    * update tests
    * fix issue with metric registration in test
    * test for unknown 'key' handling

--- src/main/java/de/sldk/mc/metrics/Entities.java
+++ src/main/java/de/sldk/mc/metrics/Entities.java
@@ -2,15 +2,38 @@ package de.sldk.mc.metrics;
 
 import io.prometheus.client.Gauge;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.Plugin;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.singletonMap;
+
+/**
+ * Get current count of all entities.
+ *
+ * Entities are labelled by
+ * <ol>
+ *     <li> world,
+ *     <li> type ({@link EntityType}),
+ *     <li> alive ({@link EntityType#isAlive()}),
+ *     <li> and spawnable ({@link EntityType#isSpawnable()})
+ * </ol>
+ */
 public class Entities extends WorldMetric {
 
     private static final Gauge ENTITIES = Gauge.build()
             .name(prefix("entities_total"))
             .help("Entities loaded per world")
-            .labelNames("world")
+            .labelNames("world", "type", "alive", "spawnable")
             .create();
+
+    /**
+     * Override the value returned by {@link EntityType#isAlive()}.
+     */
+    private static final Map<EntityType, Boolean> ALIVE_OVERRIDE = singletonMap(EntityType.ARMOR_STAND, false);
 
     public Entities(Plugin plugin) {
         super(plugin, ENTITIES);
@@ -18,6 +41,29 @@ public class Entities extends WorldMetric {
 
     @Override
     public void collect(World world) {
-        ENTITIES.labels(world.getName()).set(world.getEntities().size());
+        Map<EntityType, Long> mapEntityTypesToCounts = world.getEntities().stream()
+                .collect(Collectors.groupingBy(Entity::getType, Collectors.counting()));
+
+        mapEntityTypesToCounts.keySet()
+                .forEach(entityType ->
+                        ENTITIES
+                                .labels(world.getName(),
+                                        getEntityName(entityType),
+                                        Boolean.toString(isEntityTypeAlive(entityType)),
+                                        Boolean.toString(entityType.isSpawnable()))
+                                .set(mapEntityTypesToCounts.get(entityType))
+                );
+    }
+
+    private String getEntityName(EntityType type) {
+        try {
+            return type.getKey().getKey();
+        } catch (IllegalArgumentException e) {
+            return type.name();
+        }
+    }
+
+    private boolean isEntityTypeAlive(EntityType type) {
+        return ALIVE_OVERRIDE.containsKey(type) ? ALIVE_OVERRIDE.get(type) : type.isAlive();
     }
 }
---
+++ src/main/test/de/sldk/mc/metrics/EntitiesTest.java
@@ -0,0 +1,144 @@
+package de.sldk.mc.metrics;
+
+import io.prometheus.client.CollectorRegistry;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.plugin.Plugin;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class EntitiesTest {
+
+    private static final String ENTITY_METRIC_NAME = "mc_entities_total";
+    private static final String[] METRIC_LABELS = new String[]{"world", "type", "alive", "spawnable"};
+
+    private Entities entitiesMetric;
+
+    @BeforeAll
+    static void beforeAllTests() {
+        CollectorRegistry.defaultRegistry.clear();
+    }
+
+    @BeforeEach
+    void beforeEachTest() {
+        entitiesMetric = new Entities(mock(Plugin.class));
+        entitiesMetric.enable();
+    }
+
+    @AfterEach
+    void afterEachTest() {
+        CollectorRegistry.defaultRegistry.clear();
+    }
+
+    @Test
+    void givenTypedEntitiesExpectCorrectCount() {
+        final String worldName = "world_name";
+        final long numOfPigs = 1;
+        final long numOfHorses = 6;
+        final long numOfOrbs = 800;
+        final long numOfChicken = 9000;
+        final long numOfMinecarts = 10000;
+        List<Entity> mockedEntities = new ArrayList<>();
+        mockedEntities.addAll(mockEntities(numOfPigs, EntityType.PIG));
+        mockedEntities.addAll(mockEntities(numOfHorses, EntityType.HORSE));
+        mockedEntities.addAll(mockEntities(numOfOrbs, EntityType.EXPERIENCE_ORB));
+        mockedEntities.addAll(mockEntities(numOfChicken, EntityType.CHICKEN));
+        mockedEntities.addAll(mockEntities(numOfMinecarts, EntityType.MINECART));
+        Collections.shuffle(mockedEntities);
+
+        World world = mock(World.class);
+        when(world.getName()).thenReturn(worldName);
+        when(world.getEntities()).thenReturn(mockedEntities);
+
+        entitiesMetric.collect(world);
+
+        assertEquals(numOfPigs,
+                CollectorRegistry.defaultRegistry
+                        .getSampleValue(ENTITY_METRIC_NAME,
+                                METRIC_LABELS,
+                                new String[]{worldName, "pig", "true", "true"}));
+
+        assertEquals(numOfHorses,
+                CollectorRegistry.defaultRegistry
+                        .getSampleValue(ENTITY_METRIC_NAME,
+                                METRIC_LABELS,
+                                new String[]{worldName, "horse", "true", "true"}));
+
+        assertEquals(numOfOrbs,
+                CollectorRegistry.defaultRegistry
+                        .getSampleValue(ENTITY_METRIC_NAME,
+                                METRIC_LABELS,
+                                new String[]{worldName, "experience_orb", "false", "true"}));
+
+        assertEquals(numOfChicken,
+                CollectorRegistry.defaultRegistry
+                        .getSampleValue(ENTITY_METRIC_NAME,
+                                METRIC_LABELS,
+                                new String[]{worldName, "chicken", "true", "true"}));
+
+        assertEquals(numOfMinecarts,
+                CollectorRegistry.defaultRegistry
+                        .getSampleValue(ENTITY_METRIC_NAME,
+                                METRIC_LABELS,
+                                new String[]{worldName, "minecart", "false", "true"}));
+    }
+
+    @Test
+    void expectArmorStandAliveToBeFalse() {
+        final String worldName = "world_name";
+        final long numOfArmorStands = 11;
+        List<Entity> mockedEntities = new ArrayList<>(mockEntities(numOfArmorStands, EntityType.ARMOR_STAND));
+
+        World world = mock(World.class);
+        when(world.getName()).thenReturn(worldName);
+        when(world.getEntities()).thenReturn(mockedEntities);
+
+        entitiesMetric.collect(world);
+
+        assertEquals(numOfArmorStands,
+                CollectorRegistry.defaultRegistry
+                        .getSampleValue(ENTITY_METRIC_NAME,
+                                METRIC_LABELS,
+                                new String[]{worldName, "armor_stand", "false", "true"}));
+    }
+
+    @Test
+    void givenUnknownTypeExpectNoError() {
+        final String worldName = "world_name";
+        final long numOfUnknowns = 33;
+        List<Entity> mockedEntities = new ArrayList<>(mockEntities(numOfUnknowns, EntityType.UNKNOWN));
+
+        World world = mock(World.class);
+        when(world.getName()).thenReturn(worldName);
+        when(world.getEntities()).thenReturn(mockedEntities);
+
+        entitiesMetric.collect(world);
+
+        assertEquals(numOfUnknowns,
+                CollectorRegistry.defaultRegistry
+                        .getSampleValue(ENTITY_METRIC_NAME,
+                                METRIC_LABELS,
+                                new String[]{worldName, "UNKNOWN", "false", "false"}));
+    }
+
+    private List<Entity> mockEntities(long count, EntityType type) {
+        return LongStream.range(0, count).mapToObj(i -> mockEntity(type)).collect(Collectors.toList());
+    }
+
+    private Entity mockEntity(EntityType type) {
+        Entity e = mock(Entity.class);
+        when(e.getType()).thenReturn(type);
+        return e;
+    }
+}
