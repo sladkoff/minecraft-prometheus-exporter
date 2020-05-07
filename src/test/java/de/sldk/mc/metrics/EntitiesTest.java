b8f88370cbba2e7a3db6b3418c252c05271e0755
Leonid Koftun <leonid.koftun@device-insight.com>
2020-05-07 23:24:57 +0200

    Run formatter, tabs to spaces

--- src/test/java/de/sldk/mc/metrics/EntitiesTest.java
+++ src/test/java/de/sldk/mc/metrics/EntitiesTest.java
@@ -22,105 +22,105 @@ import java.util.stream.LongStream;
 
 class EntitiesTest {
 
-    private static final String ENTITY_METRIC_NAME = "mc_entities_total";
-    private static final String[] METRIC_LABELS = new String[]{"world", "type", "alive", "spawnable"};
+	private static final String ENTITY_METRIC_NAME = "mc_entities_total";
+	private static final String[] METRIC_LABELS = new String[] {"world", "type", "alive", "spawnable"};
 
-    private Entities entitiesMetric;
+	private Entities entitiesMetric;
 
-    @BeforeAll
-    static void beforeAllTests() {
-        CollectorRegistry.defaultRegistry.clear();
-    }
+	@BeforeAll
+	static void beforeAllTests() {
+		CollectorRegistry.defaultRegistry.clear();
+	}
 
-    @BeforeEach
-    void beforeEachTest() {
-        entitiesMetric = new Entities(mock(Plugin.class));
-        entitiesMetric.enable();
-    }
+	@BeforeEach
+	void beforeEachTest() {
+		entitiesMetric = new Entities(mock(Plugin.class));
+		entitiesMetric.enable();
+	}
 
-    @AfterEach
-    void afterEachTest() {
-        CollectorRegistry.defaultRegistry.clear();
-    }
+	@AfterEach
+	void afterEachTest() {
+		CollectorRegistry.defaultRegistry.clear();
+	}
 
-    @Test
-    void givenTypedEntitiesExpectCorrectCount() {
-        final String worldName = "world_name";
-        final long numOfPigs = 1;
-        final long numOfHorses = 6;
-        final long numOfOrbs = 800;
-        final long numOfChicken = 9000;
-        final long numOfMinecarts = 10000;
-        List<Entity> mockedEntities = new ArrayList<>();
-        mockedEntities.addAll(mockEntities(numOfPigs, EntityType.PIG));
-        mockedEntities.addAll(mockEntities(numOfHorses, EntityType.HORSE));
-        mockedEntities.addAll(mockEntities(numOfOrbs, EntityType.EXPERIENCE_ORB));
-        mockedEntities.addAll(mockEntities(numOfChicken, EntityType.CHICKEN));
-        mockedEntities.addAll(mockEntities(numOfMinecarts, EntityType.MINECART));
-        Collections.shuffle(mockedEntities);
+	@Test
+	void givenTypedEntitiesExpectCorrectCount() {
+		final String worldName = "world_name";
+		final long numOfPigs = 1;
+		final long numOfHorses = 6;
+		final long numOfOrbs = 800;
+		final long numOfChicken = 9000;
+		final long numOfMinecarts = 10000;
+		List<Entity> mockedEntities = new ArrayList<>();
+		mockedEntities.addAll(mockEntities(numOfPigs, EntityType.PIG));
+		mockedEntities.addAll(mockEntities(numOfHorses, EntityType.HORSE));
+		mockedEntities.addAll(mockEntities(numOfOrbs, EntityType.EXPERIENCE_ORB));
+		mockedEntities.addAll(mockEntities(numOfChicken, EntityType.CHICKEN));
+		mockedEntities.addAll(mockEntities(numOfMinecarts, EntityType.MINECART));
+		Collections.shuffle(mockedEntities);
 
-        World world = mock(World.class);
-        when(world.getName()).thenReturn(worldName);
-        when(world.getEntities()).thenReturn(mockedEntities);
+		World world = mock(World.class);
+		when(world.getName()).thenReturn(worldName);
+		when(world.getEntities()).thenReturn(mockedEntities);
 
-        entitiesMetric.collect(world);
+		entitiesMetric.collect(world);
 
-        assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
-                new String[] {worldName, "pig", "true", "true"})).isEqualTo(numOfPigs);
+		assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
+				new String[] {worldName, "pig", "true", "true"})).isEqualTo(numOfPigs);
 
 
-        assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
-                new String[] {worldName, "horse", "true", "true"})).isEqualTo(numOfHorses);
+		assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
+				new String[] {worldName, "horse", "true", "true"})).isEqualTo(numOfHorses);
 
-        assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
-                new String[] {worldName, "experience_orb", "false", "true"})).isEqualTo(numOfOrbs);
+		assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
+				new String[] {worldName, "experience_orb", "false", "true"})).isEqualTo(numOfOrbs);
 
-        assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
-                new String[] {worldName, "chicken", "true", "true"})).isEqualTo(numOfChicken);
+		assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
+				new String[] {worldName, "chicken", "true", "true"})).isEqualTo(numOfChicken);
 
-        assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
-                new String[] {worldName, "minecart", "false", "true"})).isEqualTo(numOfMinecarts);
-    }
+		assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
+				new String[] {worldName, "minecart", "false", "true"})).isEqualTo(numOfMinecarts);
+	}
 
-    @Test
-    void expectArmorStandAliveToBeFalse() {
-        final String worldName = "world_name";
-        final long numOfArmorStands = 11;
-        List<Entity> mockedEntities = new ArrayList<>(mockEntities(numOfArmorStands, EntityType.ARMOR_STAND));
+	@Test
+	void expectArmorStandAliveToBeFalse() {
+		final String worldName = "world_name";
+		final long numOfArmorStands = 11;
+		List<Entity> mockedEntities = new ArrayList<>(mockEntities(numOfArmorStands, EntityType.ARMOR_STAND));
 
-        World world = mock(World.class);
-        when(world.getName()).thenReturn(worldName);
-        when(world.getEntities()).thenReturn(mockedEntities);
+		World world = mock(World.class);
+		when(world.getName()).thenReturn(worldName);
+		when(world.getEntities()).thenReturn(mockedEntities);
 
-        entitiesMetric.collect(world);
+		entitiesMetric.collect(world);
 
-        assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
-                new String[] {worldName, "armor_stand", "false", "true"})).isEqualTo(numOfArmorStands);
-    }
+		assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
+				new String[] {worldName, "armor_stand", "false", "true"})).isEqualTo(numOfArmorStands);
+	}
 
-    @Test
-    void givenUnknownTypeExpectNoError() {
-        final String worldName = "world_name";
-        final long numOfUnknowns = 33;
-        List<Entity> mockedEntities = new ArrayList<>(mockEntities(numOfUnknowns, EntityType.UNKNOWN));
+	@Test
+	void givenUnknownTypeExpectNoError() {
+		final String worldName = "world_name";
+		final long numOfUnknowns = 33;
+		List<Entity> mockedEntities = new ArrayList<>(mockEntities(numOfUnknowns, EntityType.UNKNOWN));
 
-        World world = mock(World.class);
-        when(world.getName()).thenReturn(worldName);
-        when(world.getEntities()).thenReturn(mockedEntities);
+		World world = mock(World.class);
+		when(world.getName()).thenReturn(worldName);
+		when(world.getEntities()).thenReturn(mockedEntities);
 
-        entitiesMetric.collect(world);
+		entitiesMetric.collect(world);
 
-        assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
-                new String[] {worldName, "UNKNOWN", "false", "false"})).isEqualTo(numOfUnknowns);
-    }
+		assertThat(CollectorRegistry.defaultRegistry.getSampleValue(ENTITY_METRIC_NAME, METRIC_LABELS,
+				new String[] {worldName, "UNKNOWN", "false", "false"})).isEqualTo(numOfUnknowns);
+	}
 
-    private List<Entity> mockEntities(long count, EntityType type) {
-        return LongStream.range(0, count).mapToObj(i -> mockEntity(type)).collect(Collectors.toList());
-    }
+	private List<Entity> mockEntities(long count, EntityType type) {
+		return LongStream.range(0, count).mapToObj(i -> mockEntity(type)).collect(Collectors.toList());
+	}
 
-    private Entity mockEntity(EntityType type) {
-        Entity e = mock(Entity.class);
-        when(e.getType()).thenReturn(type);
-        return e;
-    }
+	private Entity mockEntity(EntityType type) {
+		Entity e = mock(Entity.class);
+		when(e.getType()).thenReturn(type);
+		return e;
+	}
 }
--- src/test/java/de/sldk/mc/metrics/VillagersTest.java
+++ src/test/java/de/sldk/mc/metrics/VillagersTest.java
@@ -19,55 +19,54 @@ import java.util.stream.Stream;
 
 class VillagersTest {
 
-    private static final String VILLAGERS_METRIC_NAME = "mc_villagers_total";
-    private static final String[] METRIC_LABELS = new String[]{"world", "type", "profession", "level"};
+	private static final String VILLAGERS_METRIC_NAME = "mc_villagers_total";
+	private static final String[] METRIC_LABELS = new String[] {"world", "type", "profession", "level"};
 
-    private static final CollectorRegistry REGISTRY = CollectorRegistry.defaultRegistry;
-    private Villagers villagersMetric;
-    private World world;
+	private static final CollectorRegistry REGISTRY = CollectorRegistry.defaultRegistry;
+	private Villagers villagersMetric;
+	private World world;
 
-    @BeforeEach
-    void beforeEachTest() {
-        REGISTRY.clear();
-        Plugin plugin = mock(Plugin.class);
-        villagersMetric = new Villagers(plugin);
-        villagersMetric.enable();
-        world = mock(World.class);
-    }
+	@BeforeEach
+	void beforeEachTest() {
+		REGISTRY.clear();
+		Plugin plugin = mock(Plugin.class);
+		villagersMetric = new Villagers(plugin);
+		villagersMetric.enable();
+		world = mock(World.class);
+	}
 
-    @Test
-    void givenVillagersExpectCorrectCount() {
-        final String worldName = "world_name";
-        final long numOfDesertFarmersLevel1 = 2;
-        final long numOfPlainsNoneLevel2 = 3;
+	@Test
+	void givenVillagersExpectCorrectCount() {
+		final String worldName = "world_name";
+		final long numOfDesertFarmersLevel1 = 2;
+		final long numOfPlainsNoneLevel2 = 3;
 
-        List<Villager> mockedVillagers = Stream.concat(
-            mockVillagers(numOfDesertFarmersLevel1, Villager.Type.DESERT, Villager.Profession.FARMER, 1),
-            mockVillagers(numOfPlainsNoneLevel2, Villager.Type.PLAINS, Villager.Profession.NONE, 2)
-        ).collect(Collectors.toList());
+		List<Villager> mockedVillagers = Stream.concat(
+				mockVillagers(numOfDesertFarmersLevel1, Villager.Type.DESERT, Villager.Profession.FARMER, 1),
+				mockVillagers(numOfPlainsNoneLevel2, Villager.Type.PLAINS, Villager.Profession.NONE, 2))
+				.collect(Collectors.toList());
 
-        when(world.getName()).thenReturn(worldName);
-        when(world.getEntitiesByClass(Villager.class)).thenReturn(mockedVillagers);
-        villagersMetric.collect(world);
+		when(world.getName()).thenReturn(worldName);
+		when(world.getEntitiesByClass(Villager.class)).thenReturn(mockedVillagers);
+		villagersMetric.collect(world);
 
-        assertThat(REGISTRY.getSampleValue(VILLAGERS_METRIC_NAME, METRIC_LABELS,
-                new String[] {worldName, "desert", "farmer", "1"})).isEqualTo(numOfDesertFarmersLevel1);
+		assertThat(REGISTRY.getSampleValue(VILLAGERS_METRIC_NAME, METRIC_LABELS,
+				new String[] {worldName, "desert", "farmer", "1"})).isEqualTo(numOfDesertFarmersLevel1);
 
-        assertThat(REGISTRY.getSampleValue(VILLAGERS_METRIC_NAME, METRIC_LABELS,
-                new String[] {worldName, "plains", "none", "2"})).isEqualTo(numOfPlainsNoneLevel2);
-    }
+		assertThat(REGISTRY.getSampleValue(VILLAGERS_METRIC_NAME, METRIC_LABELS,
+				new String[] {worldName, "plains", "none", "2"})).isEqualTo(numOfPlainsNoneLevel2);
+	}
 
-    private Stream<Villager> mockVillagers(long count, Villager.Type type, Villager.Profession profession, int level) {
-        return LongStream.range(0, count)
-                .mapToObj(i -> mockVillager(type, profession, level));
-    }
+	private Stream<Villager> mockVillagers(long count, Villager.Type type, Villager.Profession profession, int level) {
+		return LongStream.range(0, count).mapToObj(i -> mockVillager(type, profession, level));
+	}
 
-    private Villager mockVillager(Villager.Type type, Villager.Profession profession, int level) {
-        Villager e = mock(Villager.class);
-        when(e.getType()).thenReturn(EntityType.VILLAGER);
-        when(e.getVillagerType()).thenReturn(type);
-        when(e.getProfession()).thenReturn(profession);
-        when(e.getVillagerLevel()).thenReturn(level);
-        return e;
-    }
+	private Villager mockVillager(Villager.Type type, Villager.Profession profession, int level) {
+		Villager e = mock(Villager.class);
+		when(e.getType()).thenReturn(EntityType.VILLAGER);
+		when(e.getVillagerType()).thenReturn(type);
+		when(e.getProfession()).thenReturn(profession);
+		when(e.getVillagerLevel()).thenReturn(level);
+		return e;
+	}
 }
