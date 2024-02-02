525b06d989d2dcc4a45670c446ae23cb4a5eb3d9
Bohdan P <43506080+radiant-ai@users.noreply.github.com>
2024-02-02 15:11:42 -0600

    Add support for async metrics, make world size the first async metric (#220)
    * add support for async metrics, make world size the first async metric
    
    * clean up execution logic a bit

--- src/main/java/de/sldk/mc/metrics/Metric.java
+++ src/main/java/de/sldk/mc/metrics/Metric.java
@@ -2,8 +2,11 @@ package de.sldk.mc.metrics;
 
 import io.prometheus.client.Collector;
 import io.prometheus.client.CollectorRegistry;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
 public abstract class Metric {
@@ -24,20 +27,55 @@ public abstract class Metric {
         return plugin;
     }
 
-    public void collect() {
+    public CompletableFuture<Void> collect() {
+        return CompletableFuture.runAsync(() -> {
+            if (!enabled) {
+                return;
+            }
 
-        if (!enabled) {
-            return;
-        }
+            /* If metric is capable of async execution run it on a thread pool */
+            if (isAsyncCapable()) {
 
-        try {
-            doCollect();
-        } catch (Exception e) {
-            logException(e);
-        }
+                try {
+                    doCollect();
+                }
+                catch (Exception e) {
+                    logException(e);
+                }
+                return;
+            }
+
+            /*
+            * Otherwise run the metric on the main thread and make the
+            * thread on thread pool wait for completion
+            */
+            try {
+                Bukkit.getScheduler().callSyncMethod(plugin, () -> {
+                    try {
+                        doCollect();
+                    }
+                    catch (Exception e) {
+                        logException(e);
+                    }
+                    return null;
+                }).get();
+            } catch (InterruptedException | ExecutionException e) {
+                logException(e);
+            }
+        });
     }
 
     protected abstract void doCollect();
+
+    /**
+     * This method is called during the Metric collection
+     * to determine if it is safe to do it async.
+     * By default, all Metrics are sync unless this method
+     * is overridden.
+     */
+    protected boolean isAsyncCapable() {
+        return false;
+    }
 
     private void logException(Exception e) {
         final Logger log = plugin.getLogger();
